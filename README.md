847031edc1ee26b0c7e14901cbb3640019d49496
Akula Veereshwar <veereshwar124@gmail.com>
2024-07-01 19:38:09 +0530

    first-commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Plotting Global Temperature Data from NASA
+I have took the CSV file data from the officail website of NASA where it shows the change in trends in the global average temperatures from the year 1880 - 2023.  
+
+I have done this by using a javaScript library called **Chart.js**, where we can plot different types of Graphs such as bar graphs, line graphs and Area charts etc.  
+
+Visualizing this data shows us how the global average temperature is raising year on year, which raises concern to overall humanity. 
---
+++ index.js
@@ -0,0 +1,74 @@
+
+tempChart();
+
+async function tempChart(){
+    const ctx = document.getElementById('chart').getContext('2d');
+        const dataOutput = await tempData();
+        const chart = new Chart(ctx, {
+          autoPadding: true,
+          type: 'line',
+          data: {
+            labels: dataOutput.x_axis,
+            datasets: [
+              {
+                label: 'Global Average Temperature',
+                data: dataOutput.y_axis,
+                fill: false,
+                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderWidth: 1
+              },
+              {
+                label: 'Northern Hemisphere Average Temperature',
+                data: dataOutput.northernTemp,
+                fill: false,
+                borderColor: 'rgba(52, 152, 219, 1)',
+                backgroundColor: 'rgba(52, 152, 219, 0.5)',
+                borderWidth: 1
+              },
+              {
+                label: 'Northern Hemisphere Average Temperature',
+                data: dataOutput.southernTemp,
+                fill: false,
+                borderColor: 'rgba(88, 214, 141, 1)',
+                backgroundColor: 'rgba(88, 214, 141, 0.5)',
+                borderWidth: 1
+              }
+            ]
+
+          },options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    ticks: {
+                        callback: function(value, index, ticks) {
+                            return value+"℃";
+                        }
+                    }
+                }
+            }
+        }
+        });
+}
+async function tempData() {
+
+    const x_axis = [];
+    const y_axis = [];
+    const northernTemp = [];
+    const southernTemp = [];
+    const data = await fetch('ZonAnn.Ts+dSST.csv');
+    const response = await data.text();
+    const table = response.split('\n').splice(1);
+
+    table.forEach( row => {
+        const Col = row.split(',');
+        const year = Col[0];
+        x_axis.push(year);
+        y_axis.push(parseFloat(Col[1])+14);
+        northernTemp.push(parseFloat(Col[2])+14);
+        southernTemp.push(parseFloat(Col[3])+14);
+    });
+
+    return {x_axis, y_axis, northernTemp, southernTemp};
+}
